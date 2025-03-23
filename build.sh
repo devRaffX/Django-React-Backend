eaeb8c687973c294fb46f0a05ae392c7995ba84e
raaaaaaaaaf <rridad@ssct.edu.ph>
2025-03-23 19:05:55 +0800

    Try Fix in build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
+#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]]
-then
+if [ "$CREATE_SUPERUSER" = "true" ]; then
+    sleep 5
     python manage.py createsuperuser --no-input
 fi
