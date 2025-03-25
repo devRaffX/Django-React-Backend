5a9a398ba0a5b57c86b55ff9c4118132acfa25c6
raaaaaaaaaf <rridad@ssct.edu.ph>
2025-03-25 14:05:48 +0800

    added edit function

--- api/views.py
+++ api/views.py
@@ -33,3 +33,11 @@ class NoteDelete(generics.DestroyAPIView):
         user = self.request.user
         return Note.objects.filter(author=user)
     
+class NoteEdit(generics.UpdateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+    
